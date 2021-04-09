c5cedeb79f35e1ab044e0ba19280966ec6941bde
taibuharoub <haroubtaibu@gmail.com>
2021-04-09 17:11:39 +0300

    Products and Category models

--- models/Category.js
+++ models/Category.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose")
 const { Schema } = mongoose;
 
 const categorySchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String
+    },
+    color: {
+        type: String
+    }
 })
 
 module.exports = mongoose.model("Category", categorySchema);
