845b1824570421c01541589cfafebf6f4f8f106d
taibuharoub <haroubtaibu@gmail.com>
2021-04-09 18:40:42 +0300

    Adding Category Controllers

--- routes/catgories.js
+++ routes/catgories.js
@@ -1,16 +1,80 @@
 const express = require("express");
 
-const Category = require("../models/Category")
+const Category = require("../models/Category");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-    const categoryList = await Category.find();
+  const categoryList = await Category.find();
 
-    if(!categoryList) {
-        res.status(500).json({success: false})
+  if (!categoryList) {
+    res.status(500).json({ success: false });
+  }
+  res.status(200).send(categoryList);
+});
+
+router.get("/:id", async(req, res, next) => {
+    const category = await Category.findById(req.params.id);
+
+    if(!category) {
+        return res.status(500).json({
+            message: `The category with the Id ${req.params.id} not found `
+        })
     }
-    res.send(categoryList)
+    res.status(200).send(category)
 })
+
+router.post("/", async (req, res, next) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  category = await category.save();
+
+  if (!category) return res.status(404).send("The category cannot be created");
+
+  res.send(category);
+});
+
+router.put("/:id", async (req, res, next) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        {new : true}
+    )
+
+    if (!category) return res.status(404).send("The category cannot be created");
+
+    res.send(category);
+})
+
+router.delete("/:id", (req, res, next) => {
+  Category.findByIdAndRemove(req.params.id)
+    .then(category => {
+        if(category) {
+            return res.status(200).json({
+                success: true,
+                message: "the Category has been deleted!"
+            })
+        } else {
+            return res.status(404).json({
+                success: false,
+                message: "Category not found"
+            })
+        }
+    })
+    .catch((err) => {
+        return res.status(400).json({
+            success: false,
+            error: err
+        })
+    });
+});
 
 module.exports = router;
