f54f39550a28e1074f8f9839cdcb6677979ebf1f
taibuharoub <haroubtaibu@gmail.com>
2021-04-09 20:06:15 +0300

    Adding product controllers

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,15 @@
 const express = require("express");
 
 const Product = require("../models/Product");
+const Category = require("../models/Category")
 
 const router = express.Router();
 
 router.get(`/`, async (req, res, next) => {
-  const products = await Product.find()
+  // const products = await Product.find().select("name")
+  // const products = await Product.find().select("name image")
+  // const products = await Product.find().select("name image -_id")
+  const products = await Product.find().populate("category")
 
   if(!products) {
       return res.status(500).json({
@@ -16,24 +20,43 @@ router.get(`/`, async (req, res, next) => {
   res.json(products);
 });
 
-router.post(`/`, (req, res, next) => {
-  const { name, image, countInStock } = req.body;
+
+router.get(`/:id`, async (req, res, next) => {
+  const product = await Product.findById(req.params.id).populate("category")
+
+  if(!product) {
+      return res.status(500).json({
+          success: false
+      })
+  }
+  res.json(product);
+});
+
+router.post(`/`, async (req, res, next) => {
+  // const { name, description, image, countInStock } = req.body;
+
+  const category = await Category.findById(req.body.category);
+  if(!category) return res.status(400).send("Invalid Category")
+
   const product = new Product({
-    name,
-    image,
-    countInStock,
+    name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
-  product
-    .save()
-    .then((product) => {
-      res.status(201).json(product);
-    })
-    .catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    });
+  
+  product = await product.save();
+
+  if(!product) return res.status(500).send("The product cannot be created")
+
+  res.status(200).send(product);
 });
 
 
