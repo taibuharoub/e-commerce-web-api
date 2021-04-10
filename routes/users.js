5e67bd8c38b3186e513b7883a0bd5522bba29c48
taibuharoub <haroubtaibu@gmail.com>
2021-04-10 20:40:19 +0300

    Adding User Controllers

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,25 @@ router.get("/", async (req, res, next) => {
     res.send(userList)
 })
 
+router.post("/", async (req, res, next) => {
+    let user = new User ({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send("The user cant be created")
+
+    res.send(user);
+})
+
 module.exports = router;
